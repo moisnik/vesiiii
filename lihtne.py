3d31fd1dec0aeb550f6e531e7c8cf0048611a3b2
moisnik <mia.moisnik@ut.ee>
2024-11-12 15:02:57 +0200

    tegelesin natuke vormistusega ka

--- lihtne.py
+++ lihtne.py
@@ -1,10 +1,32 @@
+################################################
+# Programmeerimine I
+# 2024/2025 sügissemester
+#
+# Projekt
+# Teema: Bingomäng
+#
+#
+# Autorid: Mia Mõisnik, Elo Liisbet Palmsaar
+#
+# mõningane eeskuju: klassikaline numbrite jaotus vastavalt 75-pallise bingo reeglitele, 
+# koodi mõningane eeskuju: https://www.geeksforgeeks.org/create-bingo-game-using-python/, mõjutanud kõige rohkem funktsiooni 'uus_number'
+#
+# Mängu käik:
+#  Enne mängimist tuleb sisestada mängija nimi.
+#  Uue numbri genereerimiseks vajutada 'enter'.
+#  Mängija saab ise kontrollida, kas number on kaardil olemas ning seejärel vajutada uuesti 'enter', et kuvada uus seis.
+#  Kui kaardil saadakse kokku rida või veerg, tuleb mängijal kirjutada 'bingo' ning programm lõpetab töö.
+#  Muul juhul tuleb peale kaardi kuvamist vajutada uuesti 'enter'.
+#
+##################################################
+
+
 import random
 
-# inspiratsioon, tugi, spikker : https://www.geeksforgeeks.org/create-bingo-game-using-python/
 
 # numbri genereerimine
 def uus_number(olnud_numbrid):
-    kõik_numbrid = set(range(1, 75))
+    kõik_numbrid = set(range(1, 76))
     numbreid_alles = kõik_numbrid - olnud_numbrid
     if numbreid_alles == {}:
         return None
@@ -13,6 +35,7 @@ def uus_number(olnud_numbrid):
         olnud_numbrid.add(number)
         return str(number)
  
+
 # kaardi genereerimine
 def bingokaart():
     kaart = [['B', 'I', 'N', 'G', 'O']]
@@ -70,16 +93,17 @@ def bingokaart():
         kaart.append(rea_numbrid)
     return kaart
 
+
 # mängimine
-def mängi():
+def main():
     nimi = input('Mängija nimi: ')
     print(f'Tere, {nimi}! Siin on sinu bingokaart:')
-    for rida in bingokaart():
+    kaart = bingokaart()
+    for rida in kaart:
         print('\t'.join(rida))
     
-    kaart = bingokaart()
     olnud_numbrid = set()
-    while i < 7:
+    while True:
         input('Numbri genereerimiseks vajuta Enter')
         number = uus_number(olnud_numbrid)
         if number == None:
@@ -87,17 +111,23 @@ def mängi():
             break
         print(f'uus number on {number}')
         
+        input()
+        number_oli = 0
         for i in range(1, 6):
             for j in range(5):
                 if kaart[i][j] == number:
                     kaart[i][j] = 'X'
+                    number_oli += 1
                     for rida in kaart:
                         print('\t'.join(rida))
+        if number_oli == 0:
+            print('Seis jääb samaks!')
+            for rida in kaart:
+                        print('\t'.join(rida))
+        
+        if input() == 'bingo':
+            break
+
     
-
-
-
-
-
-
-mängi()
+if __name__ == '__main__':
+    main()
